7aae4c1a9c312c06d2a2c0e0932067d3d750158e
Alexis <dulantoariasalexis50@gmail.com>
2023-04-01 18:19:04 -0500

    Corrigiendo Modal

--- public/scripts/logic.js
+++ public/scripts/logic.js
@@ -46,9 +46,4 @@ export const resetAll = () => {
   $contentCards.innerHTML = '';
   // Reiniciar el contador
   count = 0;
-<<<<<<< HEAD
 };
-=======
-};
-
->>>>>>> 8f5c8aa955086c844eeaa85536dc31923dff0153
